bd66ceb6922779abdc3b378859e118e79bc1e09a
j-navesx <j.naves@campus.fct.unl.pt>
2021-12-02 10:29:50 +0000

    debugging

--- map-reduce/P2/Map2A.py
+++ map-reduce/P2/Map2A.py
@@ -1,10 +1,11 @@
+
 # Map-Reduce written in python to rank counties based on air quality
 # The strategy for this will be to generate tuples of county codes(key) and arithmetic mean value of pollutant values
 # We ignore the units(ppb and ug/m^3) and compare the values directly since we don't have the M(molecular weight) and temperature
 
 import sys
 file = sys.stdin
-next(file) # Skips the header line
+file.readline() # Skips the header line
 for line in file:
     line = line.strip()
     words = line.split(',')
