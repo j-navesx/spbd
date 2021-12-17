d3784e32aa705fdb6d34b8200db0f0c84662cb74
António Mendes <95042431+T0ns1@users.noreply.github.com>
2021-12-17 08:32:17 +0000

    Debugging
    Key now properly identifies unique monitors

--- spark/P1/SPARK_P1.py
+++ spark/P1/SPARK_P1.py
@@ -9,11 +9,13 @@ try:
                      .filter( lambda x: x[1] > 0) \
                      .map(lambda x: x[0]) \
                      .map( lambda line: line.split(',')) \
-                     .map( lambda arr: (arr[24], 1))                    
-    ocurrences = logTuples.reduceByKey(sum) \
-                          .sortBy( lambda x: x[1], ascending = False) 
-    for (k,v) in ocurrences.take(10):
-        print(k,v)
+                     .map( lambda arr: ( (arr[0], arr[1], arr[2]), arr[24]) )                    
+    DistinctTuples = logTuples.distinct()
+    MonitorCounts = DistinctTuples.map( lambda x: (x[1], 1) ) \
+                                  .reduceByKey(sum) \
+                                  .sortBy( lambda x: x[1], ascending = False)
+    for row in MonitorCounts.collect():
+        print(row)
     sc.stop()
 except Exception as err:
     print(err)
