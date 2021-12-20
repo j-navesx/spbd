f0bdec210cbd1296322468acff932a085486f1ac
ro-afonso <ro.afonso@campus.fct.unl.pt>
2021-12-20 16:43:19 +0000

    Joins changed to FullOuterJoins

--- spark/spark_P5.py
+++ spark/spark_P5.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[117]:
-
-
-#!/usr/bin/env python
 import pyspark
 
 totalExecTime = 0
@@ -13,14 +7,26 @@ try :
     lines = sc.textFile('epa_hap_daily_summary-small.csv')
     usa_states_lines = sc.textFile('usa_states.csv')
 
-    logTuples = lines.filter( lambda line: len(line) > 0)                      .zipWithIndex()                      .filter( lambda x: x[1] > 0)                      .map(lambda x: x[0])                      .map( lambda line: line.split(','))                      .map( lambda arr: (arr[24], (float(arr[5]),float(arr[6])) ))                      .distinct()\ #ignore repeated entries with the same state, lat and lon
-                     .sortByKey()   
+    logTuples = lines.filter( lambda line: len(line) > 0) \
+                     .zipWithIndex() \
+                     .filter( lambda x: x[1] > 0) \
+                     .map(lambda x: x[0]) \
+                     .map( lambda line: line.split(',')) \
+                     .map( lambda arr: (arr[24], (float(arr[5]),float(arr[6])) )) \
+                     .distinct() #ignore repeated entries with the same state, lat and lon
+                        
                      #.map( lambda arr: ((arr[11][:4], arr[24]), float(arr[16])))
     
-    statesTuples = usa_states_lines.filter( lambda line: len(line) > 0)                      .zipWithIndex()                      .filter( lambda x: x[1] > 0)                      .map(lambda x: x[0])                      .map( lambda line: line.split(','))                      .map( lambda arr: (arr[1], [float(arr[2]),float(arr[3]),float(arr[4]),float(arr[5])] ))                     .mapValues(lambda x: ((x[0]+x[1])/2,(x[2]+x[3])/2))  #calculate midlat and midlon
+    statesTuples = usa_states_lines.filter( lambda line: len(line) > 0) \
+                     .zipWithIndex() \
+                     .filter( lambda x: x[1] > 0) \
+                     .map(lambda x: x[0]) \
+                     .map( lambda line: line.split(',')) \
+                     .map( lambda arr: (arr[1], [float(arr[2]),float(arr[3]),float(arr[4]),float(arr[5])] ))\
+                     .mapValues(lambda x: ((x[0]+x[1])/2,(x[2]+x[3])/2))  #calculate midlat and midlon
     
 
-    #for (key,value) in statesTuples.take(20):
+    #for (key,value) in logTuples.take(20):
         #print(key,value)
     
     #print('----------------')
@@ -32,15 +38,30 @@ try :
     stateCoords3rdQuad = tuplesJoined.filter(lambda x: float(x[1][0]) < float(x[1][1]) and float(x[1][2]) > float(x[1][3]))
     stateCoords4thQuad = tuplesJoined.filter(lambda x: float(x[1][0]) > float(x[1][1]) and float(x[1][2]) > float(x[1][3]))
     
-    stateMonitors1stQuad = stateCoords1stQuad.mapValues(lambda x: (1))                                              .reduceByKey(lambda a,b: a+b)                                             .mapValues(lambda x: ('NW',x))
+    stateMonitors1stQuad = stateCoords1stQuad.mapValues(lambda x: (1)) \
+                                             .reduceByKey(lambda a,b: a+b)\
+                                             .mapValues(lambda x: ('NW',x+0))
+                                             
     
-    stateMonitors2ndQuad = stateCoords2ndQuad.mapValues(lambda x: (1))                                              .reduceByKey(lambda a,b: a+b)                                             .mapValues(lambda x: ('NE',x))
+    stateMonitors2ndQuad = stateCoords2ndQuad.mapValues(lambda x: (1)) \
+                                             .reduceByKey(lambda a,b: a+b)\
+                                             .mapValues(lambda x: ('NE',x+0))
     
-    stateMonitors3rdQuad = stateCoords3rdQuad.mapValues(lambda x: (1))                                              .reduceByKey(lambda a,b: a+b)                                             .mapValues(lambda x: ('SW',x))
+    stateMonitors3rdQuad = stateCoords3rdQuad.mapValues(lambda x: (1)) \
+                                             .reduceByKey(lambda a,b: a+b)\
+                                             .mapValues(lambda x: ('SW',x+0))
     
-    stateMonitors4thQuad = stateCoords4thQuad.mapValues(lambda x: (1))                                              .reduceByKey(lambda a,b: a+b)                                             .mapValues(lambda x: ('SE',x))
+    stateMonitors4thQuad = stateCoords4thQuad.mapValues(lambda x: (1)) \
+                                             .reduceByKey(lambda a,b: a+b)\
+                                             .mapValues(lambda x: ('SE',x+0))
     
-    finalStateMonitorsPerQuad = stateMonitors1stQuad.join(stateMonitors2ndQuad)                                                    .join(stateMonitors3rdQuad)                                                    .join(stateMonitors4thQuad)                                                    .sortByKey()
+    #join won't do because some states dont #have any monitors in certain quadrants #like Florida (NW)
+    #fullOuterJoin works, setting the quadrant's value as None if there are no monitors in that same quadrant
+    
+    finalStateMonitorsPerQuad = stateMonitors1stQuad.fullOuterJoin(stateMonitors2ndQuad)\
+                                                    .fullOuterJoin(stateMonitors3rdQuad)\
+                                                    .fullOuterJoin(stateMonitors4thQuad)\
+                                                    .sortByKey()
     
     for (key,value) in finalStateMonitorsPerQuad.collect():
         print(key,value)
@@ -87,10 +108,3 @@ try :
 except Exception as e:
     print(e)
     sc.stop()
-
-
-# In[ ]:
-
-
-
-
