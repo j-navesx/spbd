a7da6f933a0b8ca3c242db825b9a13fa7e08cd05
António Mendes <95042431+T0ns1@users.noreply.github.com>
2021-12-20 21:23:45 +0000

    Update spark_p4.py
    Average function added

--- spark/P3/spark_p4.py
+++ spark/P3/spark_p4.py
@@ -3,6 +3,7 @@ from pyspark.sql import *
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from math import sqrt as math_sqrt
+from math import pow as power
 
 spark = SparkSession.builder.master('local[*]').appName('words').getOrCreate()
 sc = spark.sparkContext
@@ -31,8 +32,12 @@ try:
 
     final = logRows_states.map(lambda arr: (arr[0], (((arr[1]+arr[2])/2), ((arr[3]+arr[4])/2))))\
                     .join(logRows) \
-                    .map(lambda arr: (arr[0], math_sqrt( pow((arr[1][1][0]-arr[1][0][0])*111,2) + pow((arr[1][1][1]-arr[1][0][1])*111,2) ))) \
-                    .groupByKey() 
+                    .map(lambda arr: (arr[0], math_sqrt(power( (arr[1][1][0]-arr[1][0][0])*111,2) +power( (arr[1][1][1]-arr[1][0][1])*111,2)))) \
+                    .mapValues(lambda v: (v, 1)) \
+                    .reduceByKey(lambda a,b: (a[0]+b[0], a[1]+b[1])) \
+                    .mapValues(lambda v: v[0]/v[1]) \
+                    # .map(lambda arr: (arr[0], math_sqrt( power((arr[1][1][0]-arr[1][0][0])*111,2) + power((arr[1][1][1]-arr[1][0][1])*111,2) ))) \
+                    # .groupByKey()
 
     
     for k,v in final.take(20):
