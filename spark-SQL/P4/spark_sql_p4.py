46d61232709b7899befc8a644249c1940ebff671
António Mendes <95042431+T0ns1@users.noreply.github.com>
2021-12-23 04:34:53 +0000

    Update spark_sql_p4.py
    Fixed Query

--- spark-SQL/P4/spark_sql_p4.py
+++ spark-SQL/P4/spark_sql_p4.py
@@ -16,14 +16,14 @@ try:
                     .filter( lambda x: x[1] > 0) \
                     .map(lambda x: x[0]) \
                     .map( lambda line: line.split(',')) \
-                    .map( lambda arr : Row( stateName = arr[1], minLat = arr[2], maxLat = arr[3], \
-                                            minLon = arr[4], maxLon = arr[5]))
+                    .map( lambda arr : Row( name = arr[1], centerLat = (float(arr[2])+float(arr[3]))/2, \
+                                            centerLon = (float(arr[4])+float(arr[5]))/2))
     logRows = lines.filter( lambda line: len(line) > 0) \
                      .zipWithIndex() \
                      .filter( lambda x: x[1] > 0) \
                      .map(lambda x: x[0]) \
                      .map( lambda line: line.split(',')) \
-                     .map( lambda arr: Row( stateName = arr[24], siteNum = arr[2], countyCode = arr[0]+arr[1], lat = float(arr[5]), lon = float(arr[6]) ) )  
+                     .map( lambda arr: Row( stateName = arr[24], siteNum = arr[2], countyCode = arr[1], lat = float(arr[5]), lon = float(arr[6]) ) )  
     
     # Creates the dataframes and views
     logRowsDF = spark.createDataFrame( logRows )
@@ -31,33 +31,13 @@ try:
 
     logRows_statesDF = spark.createDataFrame( logRows_states )
     logRows_statesDF.createOrReplaceTempView("log_states")
-    
-    query = "SELECT log.name\
-        FROM (SELECT log.name,\
-            \
-        )"
-    
-    query = "SELECT stateName as State, avg(Dist_Monitor_Center) as Avg_Dist_Monitor_Center \
-            FROM (SELECT stateName,\
-                countyCode,\
-                siteNum,\
-                sqrt( pow( (lat-((minLat+maxLat)/2))*111, 2) + pow( (lon-((minLon+maxLon)/2))*111, 2) ) as Dist_Monitor_Center\
-                FROM (SELECT log.stateName,\
-                    log.countyCode,\
-                    log.siteNum,\
-                    log.lat,\
-                    log.lon,\
-                    log_states.minLat,\
-                    log_states.maxLat,\
-                    log_states.minLon,\
-                    log_states.maxLon\
-                    FROM log\
-                    JOIN log_states ON stateName = name\
-                ) table1\
-                GROUP BY stateName, countyCode, siteNum, lat, lon, minLat, maxLat, minLon, maxLon\
-            ) table2\
-            GROUP BY stateName\
-            ORDER BY Avg_Dist_Monitor_Center DESC"
+
+    query = "SELECT stateName as State, AVG(Dist_Monitor_Center) as Avg_Dist_Monitor_Center \
+    FROM ( SELECT stateName, \
+        sqrt( pow( (AVG(lat-centerLat))*111, 2) + pow( (AVG(lon-centerLon))*111, 2) ) as Dist_Monitor_Center \
+        FROM log JOIN log_states ON stateName = name\
+        GROUP BY stateName, countyCode, siteNum )\
+    GROUP BY State"
 
     finalDF = spark.sql(query)
 
